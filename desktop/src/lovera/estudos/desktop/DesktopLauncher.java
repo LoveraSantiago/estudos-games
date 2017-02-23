1f656323222da6b198d06008d13d00f3ef6850cd
Santiago Lovera <santiago.lovera@gmail.com>
2017-02-22 22:51:23 -0300

    primeira tentativa 3d

--- desktop/src/lovera/estudos/desktop/DesktopLauncher.java
+++ desktop/src/lovera/estudos/desktop/DesktopLauncher.java
@@ -2,13 +2,26 @@ package lovera.estudos.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import lovera.estudos.desktop.utils.Executavel;
 import lovera.estudos.main.MainGame;
+import lovera.estudos.tests.box2d.ApplyForce;
 import lovera.estudos.tresd.Demo3D;
 
 public class DesktopLauncher {
+
+	private static Executavel executavel = Executavel.APPLYFORCE;
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		new LwjglApplication(new MainGame(), config);
-		new LwjglApplication(new Demo3D(), config);
+
+		switch (executavel){
+			case DEMO3D:
+				new LwjglApplication(new MainGame(), config);
+				break;
+			case APPLYFORCE:
+				new LwjglApplication(new ApplyForce(), config);
+				break;
+		}
 	}
 }
---
+++ desktop/src/lovera/estudos/desktop/utils/Executavel.java
@@ -0,0 +1,7 @@
+package lovera.estudos.desktop.utils;
+
+public enum Executavel {
+
+    DEMO3D, //Demo 3d Game from scrath https://www.youtube.com/watch?v=P56YfZ_sTB8
+    APPLYFORCE; // Box2d https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/box2d/ApplyForce.java
+}
