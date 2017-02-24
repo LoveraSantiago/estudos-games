32a2a30be0cc31d44b90a6745d0515af2aed41a6
Santiago Lovera <santiago.lovera@gmail.com>
2017-02-23 22:24:00 -0300

    modelo 3d animacao

--- core/src/lovera/estudos/utils/Executavel.java
+++ core/src/lovera/estudos/utils/Executavel.java
@@ -4,5 +4,6 @@ public enum Executavel {
 
     SEMNOME,
     DEMO3D, //Demo 3d Game from scrath https://www.youtube.com/watch?v=P56YfZ_sTB8
+    MODELTEST, //Demo 3d Game https://www.youtube.com/watch?v=zUDylPWtAts&t=73s
     APPLYFORCE; // Box2d https://github.com/libgdx/libgdx/blob/master/tests/gdx-tests/src/com/badlogic/gdx/tests/box2d/ApplyForce.java
 }
--- desktop/src/lovera/estudos/desktop/DesktopLauncher.java
+++ desktop/src/lovera/estudos/desktop/DesktopLauncher.java
@@ -1,8 +1,11 @@
 package lovera.estudos.desktop;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import lovera.estudos.tresd.ModelTest;
 import lovera.estudos.utils.Executavel;
 import lovera.estudos.main.MainGame;
 import lovera.estudos.semnomeainda.principal.SemNomeMain;
@@ -10,21 +13,26 @@ import lovera.estudos.tests.box2d.ApplyForce;
 
 public class DesktopLauncher {
 
-	private static Executavel executavel = Executavel.APPLYFORCE;
+	private static Executavel executavel = Executavel.MODELTEST;
 
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		ApplicationListener app = null;
 
 		switch (executavel){
 			case DEMO3D:
-				new LwjglApplication(new MainGame(), config);
+				app = new MainGame();
+				break;
+			case MODELTEST:
+				app = new ModelTest();
 				break;
 			case APPLYFORCE:
-				new LwjglApplication(new ApplyForce(), config);
+				app = new ApplyForce();
 				break;
 			case SEMNOME:
-				new LwjglApplication(new SemNomeMain(), config);
+				app = new SemNomeMain();
 				break;
 		}
+		new LwjglApplication(app, config);
 	}
 }
