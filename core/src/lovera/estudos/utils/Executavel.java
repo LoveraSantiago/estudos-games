9be7299598e523edba68fbb3177d5c214668a16d
Santiago Lovera <santiago.lovera@gmail.com>
2017-03-01 11:30:28 -0300

    entendo mapas com o superkoalio

--- core/src/lovera/estudos/utils/Executavel.java
+++ core/src/lovera/estudos/utils/Executavel.java
@@ -2,6 +2,7 @@ package lovera.estudos.utils;
 
 public enum Executavel {
 
+    SUPER_KOALIO,
     PIXEL_SIZE, //https://xoppa.github.io/blog/pixels/
     DEMO3D, //Demo 3d Game from scrath https://www.youtube.com/watch?v=P56YfZ_sTB8
     MODELTEST, //Demo 3d Game https://www.youtube.com/watch?v=zUDylPWtAts&t=73s
--- desktop/src/lovera/estudos/desktop/DesktopLauncher.java
+++ desktop/src/lovera/estudos/desktop/DesktopLauncher.java
@@ -6,13 +6,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import lovera.estudos.main.MainGame;
 import lovera.estudos.pixelsize.LibGDXTest;
+import lovera.estudos.superkoalio.SuperKoalio;
 import lovera.estudos.tests.box2d.ApplyForce;
 import lovera.estudos.tresd.ModelTest;
 import lovera.estudos.utils.Executavel;
 
 public class DesktopLauncher {
 
-	private static Executavel executavel = Executavel.PIXEL_SIZE;
+	private static Executavel executavel = Executavel.SUPER_KOALIO;
 
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -30,6 +31,9 @@ public class DesktopLauncher {
 				break;
 			case PIXEL_SIZE:
 				app = new LibGDXTest();
+				break;
+			case SUPER_KOALIO:
+				app = new SuperKoalio();
 				break;
 		}
 		new LwjglApplication(app, config);
